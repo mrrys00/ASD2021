e0086d163020a25bd0dbc1937f4a99addf1e7962
Szymon Ryś <rysszymon00@gmail.com>
2021-03-07 15:40:07 +0100

    remove unnecessary files

--- Cw1/tests.py
+++
@@ -1,7 +0,0 @@
-from online_1_s import bubbleSort
-# from ..Utils.utils import Utils
-from ..Utils.utils import *
-
-array = Utils().randomArray()
-
-print(bubbleSort(array))
--- tsts.py
+++
@@ -1,6 +0,0 @@
-from Cw1.online_1_s import bubbleSort
-from Utils.utils import Utils
-
-array = Utils().randomArray()
-
-print(bubbleSort(array))
